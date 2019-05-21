0ebc129ed14df7baac50beb3caa50ea97b5c6b78
shihabrpi15 <shihabrpi15@yahoo.com>
2019-05-21 13:37:05 +0600

    Win App Added

---
+++ FirstWinApp/FirstWinApp/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace FirstWinApp
+{
+    partial class HomeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.showButton = new System.Windows.Forms.Button();
+            this.itemComboBox = new System.Windows.Forms.ComboBox();
+            this.textBoxFullName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Full Name :";
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(225, 161);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(113, 28);
+            this.showButton.TabIndex = 1;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // itemComboBox
+            // 
+            this.itemComboBox.FormattingEnabled = true;
+            this.itemComboBox.Items.AddRange(new object[] {
+            "Dhaka",
+            "Barisal"});
+            this.itemComboBox.Location = new System.Drawing.Point(149, 87);
+            this.itemComboBox.Name = "itemComboBox";
+            this.itemComboBox.Size = new System.Drawing.Size(189, 28);
+            this.itemComboBox.TabIndex = 2;
+            // 
+            // textBoxFullName
+            // 
+            this.textBoxFullName.Location = new System.Drawing.Point(149, 32);
+            this.textBoxFullName.Name = "textBoxFullName";
+            this.textBoxFullName.Size = new System.Drawing.Size(189, 26);
+            this.textBoxFullName.TabIndex = 3;
+            // 
+            // HomeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 319);
+            this.Controls.Add(this.textBoxFullName);
+            this.Controls.Add(this.itemComboBox);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "HomeForm";
+            this.Text = "Home";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.ComboBox itemComboBox;
+        private System.Windows.Forms.TextBox textBoxFullName;
+    }
+}
+
---
+++ FirstWinApp/FirstWinApp/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstWinApp
+{
+    public partial class HomeForm : Form
+    {
+        public HomeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("My name is "+textBoxFullName.Text+" My home district is "+itemComboBox.Text);
+        }
+    }
+}
